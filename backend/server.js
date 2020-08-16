686a9e939d86ca0ffefc6a1ec9c6619c88af019b
Will Chomrak <wchomrak@gmail.com>
2020-08-15 19:19:44 -0600

    Application error troubleshoot 8

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const mongoose = require("mongoose");
+const path = require("path");
 
 // require("dotenv").config();
 
@@ -12,6 +13,10 @@ app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(__dirname + "build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "build", "index.html"));
+  });
 }
 
 // const uri = process.env.ATLAS_URI;
